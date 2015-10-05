9c04d3d01d427a03e07f56b2aff0b7bf752766c7
Chuck Konkol <ckonkol@gmail.com>
2015-10-05 07:21:02 -0500

    update

--- MyFavBands/MyFavBands/DetailViewController.swift
+++ MyFavBands/MyFavBands/DetailViewController.swift
@@ -1,57 +1,63 @@
 //
 //  DetailViewController.swift
-//  MyFavBands
+//  MyBands
 //
-//  Created by Charles Konkol on 2/14/15.
-//  Copyright (c) 2015 Rock Valley College. All rights reserved.
+//  Created by Charles Konkol on 2015-10-05.
+//  Copyright © 2015 Chuck Konkol. All rights reserved.
 //
 
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    //Ignore Errors until you have complete code changes in DetailViewController.swift.txt
+    
+    //1) Comment out detailDescriptionLabel
+    
+    // @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
     @IBOutlet weak var productImageView: UIImageView!
-//1) Comment out detailDescriptionLabel
-    //@IBOutlet weak var detailDescriptionLabel: UILabel!
-//   
-    //initializeers
-   
+    
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
-//2) Comment out self.configureView()
-           // self.configureView()
+            //2) Comment out self.configureView()
+            //self.configureView()
         }
     }
     
-//Errors will start going away from MasterViewController
-//3) Add didSet for productName
+    //Errors will start going away from MasterViewController
+    //3) Add didSet for productName
     var productName: String? {
         didSet {
-         
+            
         }
     }
     
-//3) Add didSet for productURL
+    //4) Add didSet for productURL
     var productURL: NSString? {
         didSet {
             // Update the view.
-           // self.configureView()
+            // self.configureView()
         }
     }
-  
-//4) Modify  func configureView()
+    
+    
+    //5) Modify func configureView() Ignore warnings
+    
     func configureView() {
+        // Update the user interface for the detail item.
         // Update image if detailItem has been passed a value from MasterViewController Seque
         if let detail: AnyObject = self.detailItem {
-             productImageView.image = UIImage(named:productURL! as String)
-        
+            productImageView.image = UIImage(named:productURL! as String)
+            
         }
     }
-
-//5) Modify override func viewDidLoad()
+    
+    //6) Modify override func viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         // Do any additional setup after loading the view, typically from a nib.
         print("Product URL: \(productURL)")
         print("Product Name: \(productName)")
@@ -59,15 +65,21 @@ class DetailViewController: UIViewController {
         {
             productImageView.image = UIImage(named:"Main.png")
         }
-       title = productName
+        title = productName
         self.configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+//7) Set Constraints. Click on "ViewController" title bar in StoryBoard (until yellow circle is highlighted) and select from menu: "Editor > Resolve Layout Issues > Reset to Siggested Constraints"
+
+//8) Save
+
+//9) Compile (Command + R) & TEST
 
